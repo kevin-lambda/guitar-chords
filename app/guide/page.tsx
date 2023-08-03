7bd7a33bb7671e468630eea232832a83ad43c9e1
Kevin Lam <mtn013209@gmail.com>
2023-08-03 15:37:41 -0700

    mile: guide done

--- app/guide/page.tsx
+++ app/guide/page.tsx
@@ -45,20 +45,19 @@ export default function Help() {
         </p>
         <div className="mt-5 has-text-centered">
           <Image
-            src={"/shape_root_6.png"}
-            width={350}
-            height={300}
-            alt="diagram"
-            id="image-border"
-          />
-          <Image
-            src={"/shape_root_5.png"}
+            src={"/read_root_6.png"}
             width={350}
             height={300}
             alt="diagram"
           />
           <Image
-            src={"/shape_root_1.png"}
+            src={"/read_root_5.png"}
+            width={350}
+            height={300}
+            alt="diagram"
+          />
+          <Image
+            src={"/read_root_1.png"}
             width={350}
             height={300}
             alt="diagram"
@@ -77,33 +76,80 @@ export default function Help() {
           the major chord, and you can now <strong>play all</strong> the major
           quality for all root notes. Like F#maj, Amaj, or C#maj. For a root
           string such as the 6th string, you can keep the same shape and move up
-          or down the neck. That's why chords are defined by chord quality and
-          not specific root note chords.
+          or down the neck.
+        </p>
+
+        <div className="mt-5 has-text-centered">
+          <Image src={"/notes.png"} width={350} height={300} alt="diagram" />
+          <Image
+            src={"/6_root_majors.png"}
+            width={350}
+            height={300}
+            alt="diagram"
+          />
+          <Image
+            src={"/5_root_majors.png"}
+            width={350}
+            height={300}
+            alt="diagram"
+          />
+        </div>
+      </section>
+      <section className="section pt-5">
+        <h2 className="is-size-4 mb-3 is-family-secondary">
+          How to be efficient with chord changes distance
+        </h2>
+        <p>
+          While you can learn just one chord shape for one root string, you'll
+          need to move a lot of frets to cover all the root notes. It won't be
+          an efficient use of your movement. A recommended approach is to learn
+          the <strong>chord shapes for both 6th and 5th root strings</strong>.
+          This will cover whatever root notes you'll need to play in a
+          reasonable fret distance.
+        </p>
+        <p className="pt-3">
+          In the example below instead of moving <strong>8 frets</strong> to
+          play F major and C# major on the 6th string. The two chords can be
+          played moving <strong>1 string and 3 frets</strong>.
         </p>
 
         <div className="mt-5 has-text-centered">
           <Image
-            src={"/notes.png"}
+            src={"/F_to_Csharp_1.png"}
             width={350}
             height={300}
             alt="diagram"
-            id="image-border"
           />
           <Image
-            src={"/6shapes.png"}
+            src={"/F_to_Csharp_3.png"}
             width={350}
             height={300}
             alt="diagram"
-            id="image-border"
-          />
-          <Image
-            src={"/5shapes.png"}
-            width={350}
-            height={300}
-            alt="diagram"
-            id="image-border"
           />
         </div>
+      </section>
+      <section className="section pt-5">
+        <h2 className="is-size-4 mb-3 is-family-secondary">Next steps</h2>
+        <p>
+          Once you've learned the chord shapes for the 6th and 5th strings,
+          you've covered the basics. Try out the chord voicings for different
+          strings and see the differences in how they are expressed. The 4th and
+          1st string chord shapes are common next steps to try. Beyond that,
+          below are other resources I've found useful.
+        </p>
+        <ul className="mt-3">
+          <li>
+            <a href="https://www.fretjam.com/">Fretjam</a> - Approachable guitar
+            theory and technique guides
+          </li>
+          <li>
+            <a href="https://jguitar.com/">jGuitar</a> - Chords and scales tool
+          </li>
+          <li>
+            <a href="https://www.jazzguitar.be/">jazzguitar</a> - Jazz guitar
+            resources
+          </li>
+        </ul>
       </section>
     </div>
   )
